0d3b184b8171af5a8672f17f7044aff87bbb529a
Kristin Parrish <kristinparrish@Kristins-MacBook-Pro.local>
2020-01-23 15:47:33 -0700

    redux config added, setup store under index.js

--- tronalddump/src/index.js
+++ tronalddump/src/index.js
@@ -1,10 +1,26 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+//Config
+import configRedux from './configRedux';
+
+//Styles
 import './index.css';
+
+//App
 import App from './App';
+
+//Other
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configRedux();
+
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
