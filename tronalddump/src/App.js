a664340e69602466b30332ec85e6720471feebd7
Kristin Parrish <kristinparrish@Kristins-MacBook-Pro.local>
2020-01-23 19:10:05 -0700

    styles finalized

--- tronalddump/src/App.js
+++ tronalddump/src/App.js
@@ -8,22 +8,33 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h2>Tronald Dump</h2>
-        <h4>Random quotes from the 45th President of the United States</h4>
+        <h4
+          style={{
+          width: "75%",
+          margin: "0 auto",
+          paddingTop: "25px",
+          paddingBottom: "25px",
+          color: "white"
+          }}
+        >
+          Random generator of the "best" quotes from the 45th President of the United States</h4>
       </header>
 
       <Dumpism />
 
       <h5
-        // style={{
-        //   fontSize: "8px",
-        //   width: "200px",
-        //   margin: "0 auto",
-        //   paddingTop: "25px"
-        // }}
+        style={{
+          fontSize: "8px",
+          width: "200px",
+          margin: "0 auto",
+          paddingTop: "25px",
+          paddingBottom: "25px",
+          color: "white"
+        }}
       >
         Disclaimer: The views and opinions expressed in these quotes are those
         of the author and do not necessarily reflect this website. Quotes are
-        not vetted or fact checked and are diplayed at random.{" "}
+        not vetted or fact checked and are displayed at random.{" "}
       </h5>
     </div>
   );
--- tronalddump/src/components/Dumpism.js
+++ tronalddump/src/components/Dumpism.js
@@ -1,16 +1,60 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { actionName } from '../actions/index.js'
+import Fffr from '../images/19-trump.jpg'
+import styled from 'styled-components';
+
+const Body = styled.div`
+background-color: white;
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+padding: 5%;
+border-top: 3px orange solid;
+border-bottom: 3px orange solid; 
+`
+
+const ImgContainer = styled.div`
+display: flex;
+min-width: 35%;
+`
+const DumpismContainer = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+border: 2px solid #282c34;
+border-radius: 35px;
+min-width: 55%;
+max-width: 55%;
+`
+const Text = styled.span`
+padding: 10%;
+`
+const DumpButton = styled.button`
+    background-color: #D46742;
+    border-radius: 10px;
+    color: white;
+    font-size: 1.4rem;
+    display: inline;
+`
+
+
 
 const Dumpism = props => {
     const store = useSelector(state => state.newReducer.dumpism);
     const dispatch = useDispatch();
 
     return (
-        <div>
-        <span>{store}</span>
-        <button onClick={() => dispatch (actionName())}> New Dumpism </button>
-        </div>
+        <Body>
+        <ImgContainer>
+            <img src={Fffr} alt="trump cartoon"></img>
+        </ImgContainer>
+        <DumpismContainer>
+        <Text>{store}</Text>
+        <DumpButton onClick={() => dispatch (actionName())}> New Dumpism </DumpButton>
+        </DumpismContainer>
+        </Body>
     );
 };
 
